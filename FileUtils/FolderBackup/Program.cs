b1460aa4bc8e2b62c3b93ea27b80bc10d78a83ed
Matt Callahan <matthew.callahan@outlook.com>
2020-09-03 08:39:54 -0700

    add FolderBackup console app

---
+++ FileUtils/FolderBackup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FolderBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
